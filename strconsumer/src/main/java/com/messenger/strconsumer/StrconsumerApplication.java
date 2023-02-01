67d7314ca23862e56a1203a560ec783eaa37f97b
JonasGarciaGit <jonasemprego1@gmail.com>
2023-02-01 20:15:23 -0300

    producer and consumer created

---
+++ strconsumer/src/main/java/com/messenger/strconsumer/StrconsumerApplication.java
@@ -0,0 +1,15 @@
+package com.messenger.strconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@SpringBootApplication
+public class StrconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrconsumerApplication.class, args);
+	}
+
+}
